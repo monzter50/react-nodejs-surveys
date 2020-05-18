75635cb937ea8f06e9861b4042a778cd550e6b69
jose gonzalez <josemonzter50@gmail.com>
2020-05-17 19:58:50 -0500

    changes index config routes

--- index.js
+++ index.js
@@ -30,15 +30,19 @@ app.get('/', (req, res) =>{
       bye:'Dude'
     });
 });
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
